2c04442560d2b6f26639ad618d3275dfd065960b
jiav <yabiz94@gmail.com>
2020-09-15 14:27:51 -0500

    change admin route

--- nom035/nom035/urls.py
+++ nom035/nom035/urls.py
@@ -6,7 +6,7 @@ from users.views import login_view, index_view, user_logout, _404, _500
 urlpatterns = [
     path('surveys/', include('surveys.urls', namespace="survey")),
     path('user/', include('users.urls', namespace="user")),
-    path('admin/', admin.site.urls),
+    path('management/', admin.site.urls),
     path('login/', login_view, name='login_view'),
     path('logout/', user_logout, name='user_logout'),
     path('', index_view, name='index_view'),
