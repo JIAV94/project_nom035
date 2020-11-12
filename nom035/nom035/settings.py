e5e1d276b4b741276a81db16db7ff3816358c24e
jiav <yabiz94@gmail.com>
2020-11-11 18:14:17 -0600

    Bulk insert y Requieren atencion clinica rojo

--- nom035/nom035/settings.py
+++ nom035/nom035/settings.py
@@ -31,14 +31,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'surveys.apps.SurveysConfig',
-    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'surveys.apps.SurveysConfig',
+    'users.apps.UsersConfig',
+    'import_export',
 ]
 
 MIDDLEWARE = [
--- nom035/users/admin.py
+++ nom035/users/admin.py
@@ -1,7 +1,47 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
 from .models import Employee, Company, InformationLog
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
 
 
+class UserResource(resources.ModelResource):
+    class Meta:
+        model = User
+        fields = ('id', 'password', 'username', 'first_name', 
+                  'last_name', 'email')
+
+
+class UserAdmin(ImportExportModelAdmin):
+    resource_class = UserResource
+
+
+class EmployeeResource(resources.ModelResource):
+    class Meta:
+        model = Employee
+        fields = ('id', 'user', 'company', 'sex', 'birthdate')
+
+
+class EmployeeAdmin(ImportExportModelAdmin):
+    resource_class = EmployeeResource
+
+
+class InformationLogResource(resources.ModelResource):
+    class Meta:
+        model = InformationLog
+        fields = ('id', 'employee', 'civil_status',
+                  'educational_level', 'position', 'area',
+                  'position_type', 'contract_type',
+                  'personnel_type', 'working_day_type',
+                  'shift_rotation', 'current_position_time',
+                  'work_experience_time')
+
+class InformationLogAdmin(ImportExportModelAdmin):
+    resource_class = InformationLogResource
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
 admin.site.register(Company)
-admin.site.register(Employee)
-admin.site.register(InformationLog)
+admin.site.register(Employee, EmployeeAdmin)
+admin.site.register(InformationLog, InformationLogAdmin)
